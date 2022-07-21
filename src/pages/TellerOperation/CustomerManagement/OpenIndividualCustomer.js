03de5e8d7b7e789a8127fe090c40b12d8d66123d
18520058 <18520058@ms.uit.edu.vn>
2022-07-21 12:06:44 +0700

    v0.17: merge basic API

--- src/pages/TellerOperation/CustomerManagement/OpenIndividualCustomer.js
+++ src/pages/TellerOperation/CustomerManagement/OpenIndividualCustomer.js
@@ -6,8 +6,87 @@ import Select_Custom from "../../../components/Select_Custom";
 import DatePicker_Custom from "../../../components/DatePicker_Custom";
 import Button_Custom from "../../../components/Button_Custom";
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+function checkName(a, b) {
+    checkName = 0
+    a.map((data, index) => {
+        // console.log("data")
+        // console.log(data.Name)
+        // console.log("b")
+        // console.log(b)
+        if (data.Name == b)
+        {
+            checkName = data.id
+            return data.id
+            
+        }
+    })
+    return checkName
+}
 
 function OpenIndividualCustomer() {
+    
+    const [bioCity, setBioCity] = useState([]);
+    useEffect(() => {
+        const fetchDataCity = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_city_province`);
+            const data = await response.json();
+            setBioCity(data.rows);  
+        };
+        fetchDataCity();
+    }, []);
+
+    const [bioCountry, setBioCountry] = useState([]);
+    useEffect(() => {
+        const fetchDataCountry = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_country`);
+            const data = await response.json();
+            setBioCountry(data.rows);  
+        };
+        fetchDataCountry();
+    }, []);
+
+    const [bioDoctype, setBioDoctype] = useState([]);
+    useEffect(() => {
+        const fetchDataDoctype = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_doctype`);
+            const data = await response.json();
+            setBioDoctype(data.rows);  
+        };
+        fetchDataDoctype();
+    }, []);
+
+    const [bioMainSector, setBioMainSector] = useState([]);
+    useEffect(() => {
+        const fetchDataMainSector = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_sector`);
+            const data = await response.json();
+            setBioMainSector(data.rows);  
+        };
+        fetchDataMainSector();
+    }, []);
+
+    const [bioMainIndustry, setBioMainIndustry] = useState([]);
+    useEffect(() => {
+        const fetchDataMainIndustry = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_industry`);
+            const data = await response.json();
+            setBioMainIndustry(data.rows);  
+        };
+        fetchDataMainIndustry();
+    }, []);
+
+    const [bioAccountOfficer, setBioAccountOfficer] = useState([]);
+    useEffect(() => {
+        const fetchDataAccountOfficer = async () => {
+            const response = await fetch(`https://cb-be.azurewebsites.net/storage/get_account_officer`);
+            const data = await response.json();
+            setBioAccountOfficer(data.rows);  
+        };
+        fetchDataAccountOfficer();
+    }, []);
+
     
     return (
         <div>
@@ -46,20 +125,20 @@ function OpenIndividualCustomer() {
                         <TextField_Custom props1="GB Street" props2="35" props3="YES"    />
                         <TextField_Custom props1="GB Town/Dist" props2="35" props3="YES"/>  
                         <TextField_Custom props1="Mobile Phone" props2="15" props3="NO"/>  
-                        <Select_Custom props1="City/Province" props2="35" props3="city"/>
-                        <Select_Custom props1="GB Country" props2="30" props3="country"/>
-                        <Select_Custom props1="Nationality" props2="30" props3="country"/>
-                        <Select_Custom props1="Residence" props2="30" props3="country"/>
-                        <Select_Custom props1="Doc Type" props2="20" props3="doctype"/>
+                        <Select_Custom props1="City/Province" props2="35" props3="city" props4={bioCity}/>
+                        <Select_Custom props1="GB Country" props2="30" props3="country" props4={bioCountry}/>
+                        <Select_Custom props1="Nationality" props2="30" props3="country" props4={bioCountry}/>
+                        <Select_Custom props1="Residence" props2="30" props3="country" props4={bioCountry}/>
+                        <Select_Custom props1="Doc Type" props2="20" props3="doctype" props4={bioDoctype}/>
                         <TextField_Custom props1="Doc ID" props2="20" props3="YES"/>
                         <DatePicker_Custom props1="Doc Issue Date" props2="350" props3="YES"/>
                         <DatePicker_Custom props1="Doc Expiry Date" props2="350" props3="YES"/>
                         <TextField_Custom props1="Email Address" props2="35" props3="NO"/>
-                        <Select_Custom props1="Main Sector" props2="30" props3="main_sector"/>
-                        <Select_Custom props1="SubSector" props2="30" props3="sub_sector"/>
-                        <Select_Custom props1="Main Industry" props2="30" props3="main_industry"/>
-                        <Select_Custom props1="Industry" props2="30" props3="sub_industry"/> 
-                        <Select_Custom props1="Account Officer" props2="25" props3="account_officer"/>
+                        <Select_Custom props1="Main Sector" props2="30" props3="main_sector" props4={bioMainSector}/> 
+                        <Select_Custom props1="SubSector" props2="30" props3="sub_sector" props4={bioMainSector}/>
+                        <Select_Custom props1="Main Industry" props2="30" props3="main_industry" props4={bioMainSector}/>
+                        <Select_Custom props1="Industry" props2="30" props3="sub_industry" props4={bioMainIndustry}/> 
+                        <Select_Custom props1="Account Officer" props2="25" props3="account_officer" props4={bioAccountOfficer}/>
                     </div>
                     <div
                         style={{ 
@@ -76,30 +155,42 @@ function OpenIndividualCustomer() {
                             // color="success"
                             size="large"
                             // href="https://google.com"
-                            // onClick={() => {
-                            //     axios.post('https://cb-be.azurewebsites.net/customer/create_individual_customer',{
-                            //         firstName: document.getElementById('txtFirstName').value,
-                            //         lastName: document.getElementById('txtLastName').value,
-                            //         middleName: document.getElementById('txtMiddleName').value,
-                            //         GB_shortName: document.getElementById('txtGBShortName').value,
-                            //         GB_fullName: document.getElementById('txtGBFullName').value,
-                            //         GB_street: document.getElementById('txtGBStreet').value,
-                            //         cityProvince: document.getElementById('sltCity/Province').value,
-                            //         // cityProvince: '4',
-                            //         GB_country: document.getElementById('sltGBCountry').value,
+                            onClick={() => {
+                                
+                                let temp = document.getElementById('sltCity/Province').textContent.toString();
+                                // temp = temp[0] + temp[1]
 
-                            //     })
-                            //     .then(res => {
-                            //         console.log(res)
-                            //     })
-                            //     .catch(err=>{
-                            //         console.log(err)
-                            //     })
-                            // }}
+                                let temp01 = document.getElementById('sltGBCountry').textContent.toString();
+                                temp01 = temp01[0] + temp01[1]
+
+                                console.log("check name")
+                                console.log(checkName(bioCity, temp))
+                                console.log(temp)
+                                console.log(bioCity)
+
+                                axios.post('https://cb-be.azurewebsites.net/customer/create_individual_customer',{
+                                    firstName: document.getElementById('txtFirstName').value,
+                                    lastName: document.getElementById('txtLastName').value,
+                                    middleName: document.getElementById('txtMiddleName').value,
+                                    GB_shortName: document.getElementById('txtGBShortName').value,
+                                    GB_fullName: document.getElementById('txtGBFullName').value,
+                                    GB_street: document.getElementById('txtGBStreet').value,
+                                    // cityProvince: temp,
+                                    // cityProvince: '4',
+                                    // GB_country: temp01,
+
+                                })
+                                .then(res => {
+                                    console.log(res)
+                                    // console.log(temp)
+                                    // console.log(temp01)
+                                    // console.log(document.getElementById('sltGBCountry').options[document.getElementById('sltGBCountry').selectedIndex].value);  
+                                })
+                                .catch(err=>{
+                                    console.log(err)
+                                })
+                            }}
                         >
-                            {console.log(document.getElementById('sltGBCountry').value)
-                            
-                            }
                             SAVE
                         </Button>
                     </div>
