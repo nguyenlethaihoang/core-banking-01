7e2d86a58ed64670f0601ff4bc11f155d2f9ef97
18520058 <18520058@ms.uit.edu.vn>
2022-08-02 11:57:34 +0700

    v0.40: complete UI block pages

--- src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/Block_Custom/index.js
+++ src/pages/TellerOperation/AccountManagement/CurrentAndNonTermSavingAccount/Block_Custom/index.js
@@ -1,4 +1,4 @@
-import { Button, IconButton } from '@mui/material'
+import { Button, Checkbox, FormControlLabel, FormGroup, IconButton } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import Select_Custom from '../../../../../components/Select_Custom';
 import TextField_Custom from '../../../../../components/TextField_Custom';
@@ -9,6 +9,7 @@ import PrintIcon from '@mui/icons-material/Print';
 import Select_Value_Custom from '../../../../../components/Select_Value_Custom';
 import TextField_Value_Custom from '../../../../../components/TextField_Value_Custom';
 import TextField_Value_Custom_No_Data from '../../../../../components/TextField_Value_Custom_No_Data';
+import DatePicker_Custom from '../../../../../components/DatePicker_Custom';
 
 const closeOnlineData = [
     {id: 1,
@@ -19,6 +20,7 @@ const closeOnlineData = [
 ]
 
 let txtCustomerID = ""
+let txtDescription = ""
 
 function Block_Custom(props1) {
     let tmpURL = `https://cb-be.azurewebsites.net/account/debit_account/get/${props1.AccountCode}`
@@ -35,6 +37,7 @@ function Block_Custom(props1) {
     }, []);
 
 txtCustomerID = `${bioFilled.CustomerID} - ${bioFilled.GB_FullName}`  
+txtDescription = `PHONG TOA TK: ${bioFilled.id}`
 
 
   return (props1.trigger) ? 
@@ -85,18 +88,31 @@ txtCustomerID = `${bioFilled.CustomerID} - ${bioFilled.GB_FullName}`
                 <TextField_Value_Custom_No_Data props1="Customer ID" props2="35" props3="NO" props4={txtCustomerID}/>
                 <TextField_Value_Custom_No_Data props1="Account" props2="15" props3="NO" props4={bioFilled.id}/>
                 <TextField_Value_Custom_No_Data props1="Amount" props2="20" props3="YES" props4="0"/>
-                <TextField_Value_Custom_No_Data props1="Working UnclearedEntries" props2="15" props3="NO" props4="NO"/>
-                <TextField_Value_Custom_No_Data props1="Cheques OS" props2="15" props3="NO" props4="NO"/>
-                <TextField_Value_Custom_No_Data props1="Bank Cards" props2="15" props3="NO" props4="NO"/>
-                <TextField_Value_Custom_No_Data props1="CC Chgs OS" props2="25" props3="NO" props4="0"/>
-                <TextField_Value_Custom_No_Data props1="Total Credit Interest" props2="30" props3="NO" props4=""/>
-                <TextField_Value_Custom_No_Data props1="Total Debit Interest" props2="30" props3="NO" props4=""/>
-                <TextField_Value_Custom_No_Data props1="Total Charges" props2="30" props3="NO" props4=""/>
-                <TextField_Value_Custom_No_Data props1="Total VAT" props2="30" props3="NO" props4="0"/>
-                <TextField_Value_Custom_No_Data props1="Int.CAP to AC" props2="30" props3="NO" props4=""/>
-                <TextField_Value_Custom_No_Data props1="Trans Ref Next" props2="30" props3="NO" props4=""/>
             </div>
-
+            <div
+            
+            style={{ 
+                display: "flex", 
+                width: "100%", 
+                flexWrap: "wrap",
+                paddingBottom: "20px"
+            }}
+            >
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="Block Account" />
+                </FormGroup>
+            </div>
+            <div
+                style={{ 
+                    display: "flex", 
+                    width: "100%", 
+                    flexWrap: "wrap"
+                }}
+            >
+                <DatePicker_Custom props1="From Date" props2="350" props3="YES"/>
+                <DatePicker_Custom props1="To Date" props2="350" props3="YES"/>
+                <TextField_Value_Custom_No_Data props1="Description" props2="35" props3="YES" props4={txtDescription}/>
+            </div>
             <div
                 style={{ 
                     // display: "flex", 
